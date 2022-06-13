d599fbdce3cac832f0ee8fa71cf4b284c2d6b4da
tarique4a <tarique.nitjsr77@gmail.com>
2022-06-13 17:44:49 +0530

    linklist

---
+++ middle_ele_list_63.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+Node *middleNode(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
