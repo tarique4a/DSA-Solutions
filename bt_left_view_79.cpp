09426e8635a93aa3f0101fa1a79be36be3dc12cd
tarique4a <tarique.nitjsr77@gmail.com>
2022-06-14 12:29:16 +0530

    binary tree

---
+++ bt_left_view_79.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = right = NULL;
+    }
+};
+vector<int> leftView(Node *root)
+{
+    vector<int> res;
+    queue<Node *> q;
+    if (root == NULL)
+        return res;
+    q.push(root);
+    while (!q.empty())
+    {
+        int s = q.size();
+        for (int i = 0; i < s; ++i)
+        {
+            Node *temp = q.front();
+            q.pop();
+            if (i == 0)
+                res.push_back(temp->data);
+            if (temp->left)
+                q.push(temp->left);
+            if (temp->right)
+                q.push(temp->right);
+        }
+    }
+    return res;
+}
